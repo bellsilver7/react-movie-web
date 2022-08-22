b2fe6b9cb3cc22b3b921d4eef9584a613b552af5
bellsilver7 <bellsilver7@gmail.com>
2022-08-23 06:20:09 +0900

    To Do List / form, button 생성

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import { useState } from "react";
 function App() {
   const [todo, setTodo] = useState("");
   const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodo("");
+  };
+
   return (
     <div>
-      <input
-        onChange={onChange}
-        value={todo}
-        type="text"
-        placeholder="Write your to do..."
-      />
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
