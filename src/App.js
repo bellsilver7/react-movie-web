95a35d970b62b2a94ad0497067f0fbed03a1ae42
bellsilver7 <bellsilver7@gmail.com>
2022-08-24 06:42:03 +0900

    영화앱 / 로딩중 생성

--- src/App.js
+++ src/App.js
@@ -2,32 +2,10 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    setLoading(false);
   }, []);
-
-  return (
-    <div>
-      <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <ul>
-          {coins.map((coin) => (
-            <li key={coin.id}>
-              {coin.name}({coin.symbol}): ${coin.quotes.USD.price} USD
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+  return <div>{loading ? "Loading..." : null}</div>;
 }
 
 export default App;
