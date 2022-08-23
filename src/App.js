cf3b19a292f80ddd89b44896ae3a7d35c693d5ac
bellsilver7 <bellsilver7@gmail.com>
2022-08-24 06:58:00 +0900

    영화앱 / yts.mx API 호출 async / await 적용

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const getMovies = async () => {
+    const url =
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year";
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(json.data.movies);
+  };
+
   useEffect(() => {
-    fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        setLoading(false);
-      });
+    getMovies();
+    setLoading(false);
   }, []);
   return <div>{loading ? "Loading..." : null}</div>;
 }
