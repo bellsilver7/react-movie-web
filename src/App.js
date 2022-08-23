3585f6fa8e86962ec4a8641fa3817afc332e7fdb
bellsilver7 <bellsilver7@gmail.com>
2022-08-24 06:45:42 +0900

    영화앱 / yts.mx API 호출

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    setLoading(false);
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setLoading(false);
+      });
   }, []);
   return <div>{loading ? "Loading..." : null}</div>;
 }
