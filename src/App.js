2296b917058e557c613ed360d0eec9bdd4872fd4
bellsilver7 <bellsilver7@gmail.com>
2022-08-25 08:07:27 +0900

    영화앱 / 라우팅 - Home 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,37 +1,5 @@
-import { useState, useEffect } from "react";
-import Movie from "./components/Movie";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const url =
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year";
-    const response = await fetch(url);
-    const json = await response.json();
-
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>
-      {loading
-        ? "Loading..."
-        : movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-    </div>
-  );
+  return null;
 }
 
 export default App;
