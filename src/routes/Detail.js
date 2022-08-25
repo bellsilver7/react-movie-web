882fbb9ee9b899b0f41e46196baa16c382201a2b
bellsilver7 <bellsilver7@gmail.com>
2022-08-26 06:55:21 +0900

    영화앱 / 영화상세 API 호출

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 const Detail = () => {
+  const params = useParams();
+  const getMovie = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/movie_details.json?movie_id=${params.id}`
+    );
+    const json = await response.json();
+    const movie = json.data.movie;
+    console.log(movie);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+
   return <h1>Movie Detail</h1>;
 };
 
