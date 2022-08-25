521f30591db153c0a456a57633dc70dcc726e4d2
bellsilver7 <bellsilver7@gmail.com>
2022-08-26 07:25:41 +0900

    영화상세 코드 정리

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,49 +1,47 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-const Detail = () => {
+function Detail() {
+  const params = useParams();
   const [loading, setLoading] = useState(true);
   const [movie, setMovie] = useState({});
-  const params = useParams();
   const getMovie = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/movie_details.json?movie_id=${params.id}`
-    );
+    const url = `https://yts.mx/api/v2/movie_details.json?movie_id=${params.id}`;
+    const response = await fetch(url);
     const json = await response.json();
 
     setMovie(json.data.movie);
     setLoading(false);
   };
+
   useEffect(() => {
     getMovie();
+    console.log("use effect");
   }, []);
 
   return (
     <div>
-      <h1>Movie Detail</h1>
-      <div>
-        {loading ? (
-          "Loading..."
-        ) : (
+      {loading ? (
+        "Loading..."
+      ) : (
+        <div>
+          <img src={movie?.large_cover_image} alt="" />
+          <h2>{movie?.title}</h2>
           <div>
-            <img src={movie.large_cover_image} alt="" />
-            <h2>{movie.title}</h2>
-            <div>
-              <span>Downloads {movie.download_count}</span>|
-              <span>Uploaded {movie.date_uploaded}</span>|
-              <span>Runtime {movie.runtime}</span>|
-              <span>Like {movie.like_count}</span>|
-              <span>Rating {movie.rating}</span>
-            </div>
-            <div>
-              <h3>Description</h3>
-              <div>{movie.description_full}</div>
-            </div>
+            <span>Downloads {movie?.download_count}</span>|
+            <span>Uploaded {movie?.date_uploaded}</span>|
+            <span>Runtime {movie?.runtime}</span>|
+            <span>Like {movie?.like_count}</span>|
+            <span>Rating {movie?.rating}</span>
           </div>
-        )}
-      </div>
+          <div>
+            <h3>Description</h3>
+            <div>{movie?.description_full}</div>
+          </div>
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default Detail;
