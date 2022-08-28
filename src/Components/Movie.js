9b36fd3ac8e465f8587553de7e3e86fb57c72126
bellsilver7 <bellsilver7@gmail.com>
2022-08-29 07:26:39 +0900

    영화목록 CSS 적용

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -3,17 +3,37 @@ import { Link } from "react-router-dom";
 
 const Movie = ({ id, coverImg, title, summary, genres }) => {
   return (
-    <div>
-      <img src={coverImg} alt="" />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <div>{summary}</div>
-      <ul>
-        {genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
+    <div className="group relative">
+      <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+        <img
+          src={coverImg}
+          alt=""
+          className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+        />
+      </div>
+
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-gray-700">
+            <Link to={`/movie/${id}`}>
+              <span aria-hidden="true" className="absolute inset-0"></span>
+              {title}
+            </Link>
+          </h3>
+          <p className="text-sm mt-1 text-gray-500">
+            {summary.slice(0, 100) + "..."}
+          </p>
+          <ul class="flex">
+            {genres.map((g) => (
+              <li key={g} className="mr-1">
+                <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-pink-600 bg-pink-200 uppercase last:mr-0 mr-1">
+                  {g}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -18,7 +18,7 @@ function Home() {
     getMovies();
   }, []);
   return (
-    <div>
+    <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
       {loading
         ? "Loading..."
         : movies.map((movie) => (
