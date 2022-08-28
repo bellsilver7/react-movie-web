292ecd6d7e32e94aa9cb3c104b4a9f736d06309f
bellsilver7 <bellsilver7@gmail.com>
2022-08-29 07:25:44 +0900

    배경색 설정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
+  useEffect(() => {
+    // 👇 add class to body element
+    document.body.classList.add("bg-stone-800");
+    return () => {
+      // 👇️ optionally remove styles when component unmounts
+      document.body.classList.remove("bg-stone-800");
+    };
+  }, []);
   return (
-    <Router>
-      <Switch>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <div class="container mx-auto">
+      <Router>
+        <Switch>
+          <Route path="/movie/:id">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
