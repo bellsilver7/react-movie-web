01a66aa8dd6f62cd1143541a644bd1e805e5674e
bellsilver7 <bellsilver7@gmail.com>
2022-08-30 07:00:27 +0900

    영화 상세 스타일 적용

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,37 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+function separateComma(val) {
+  // remove sign if negative
+  var sign = 1;
+  if (val < 0) {
+    sign = -1;
+    val = -val;
+  }
+  // trim the number decimal point if it exists
+  let num = val.toString().includes(".")
+    ? val.toString().split(".")[0]
+    : val.toString();
+  let len = num.toString().length;
+  let result = "";
+  let count = 1;
+
+  for (let i = len - 1; i >= 0; i--) {
+    result = num.toString()[i] + result;
+    if (count % 3 === 0 && count !== 0 && i !== 0) {
+      result = "," + result;
+    }
+    count++;
+  }
+
+  // add number after decimal point
+  if (val.toString().includes(".")) {
+    result = result + "." + val.toString().split(".")[1];
+  }
+  // return result with - sign if negative
+  return sign < 0 ? "-" + result : result;
+}
+
 function Detail() {
   const params = useParams();
   const [loading, setLoading] = useState(true);
@@ -16,7 +47,6 @@ function Detail() {
 
   useEffect(() => {
     getMovie();
-    console.log("use effect");
   }, []);
 
   return (
@@ -24,19 +54,44 @@ function Detail() {
       {loading ? (
         "Loading..."
       ) : (
-        <div>
-          <img src={movie?.large_cover_image} alt="" />
-          <h2>{movie?.title}</h2>
-          <div>
-            <span>Downloads {movie?.download_count}</span>|
-            <span>Uploaded {movie?.date_uploaded}</span>|
-            <span>Runtime {movie?.runtime}</span>|
-            <span>Like {movie?.like_count}</span>|
-            <span>Rating {movie?.rating}</span>
+        <div className="flex">
+          <div className="w-full px-4">
+            <img src={movie?.large_cover_image} alt="" />
           </div>
-          <div>
-            <h3>Description</h3>
-            <div>{movie?.description_full}</div>
+          <div className="w-full px-4">
+            <h2 className="text-xl">
+              <strong>{movie?.title}</strong>
+            </h2>
+            <hr className="my-4" />
+            <div>
+              <h3 className="font-bold">Description</h3>
+              <div>{movie?.description_full}</div>
+            </div>
+            <hr className="my-4" />
+            <table className="table text-xs">
+              <tbody>
+                <tr>
+                  <td>Downloads</td>
+                  <td>{separateComma(movie?.download_count)}</td>
+                </tr>
+                <tr>
+                  <td>Uploaded</td>
+                  <td>{movie?.date_uploaded}</td>
+                </tr>
+                <tr>
+                  <td>Runtime</td>
+                  <td>{movie?.runtime}</td>
+                </tr>
+                <tr>
+                  <td>Like</td>
+                  <td>{movie?.like_count}</td>
+                </tr>
+                <tr>
+                  <td>Rating</td>
+                  <td>{movie?.rating}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       )}
