aff8731b9e6f83fcdb87e9bdbfe40607d1ce6fc4
bellsilver7 <bellsilver7@gmail.com>
2022-08-30 07:00:03 +0900

    App container 텍스트 컬러 설정

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     };
   }, []);
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto text-white">
       <Router>
         <Switch>
           <Route path="/movie/:id">
